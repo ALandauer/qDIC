d912f558709c92a0281d6ee7f1b0c68fa1755622
alexander.landauer <alexander.landauer@nist.gov>
2020-01-10 17:32:56 -0500

    Update image_eval.m

--- image_eval.m
+++ image_eval.m
@@ -17,7 +17,7 @@ function [noise_percent,spatial_res,CI_disp_mean,no_im] = image_eval(Folder,ext,
 %   noise_percent:  percentage of the full-scale range of the image format
 %                   greyscale that the sensor noise occupies
 %   spatial_res: the spatial resolution that can be expected from the
-%                algorithm, based on the noise, speckle pattern, optics,
+%                algorithm, based on the noise, speckle pattern, optics, 
 %                and other error sources.
 %   CI_disp_mean: mean confidence interval on the displacement measured.
 %                 This should be centered on zero, unless bias errors exist
@@ -25,7 +25,7 @@ function [noise_percent,spatial_res,CI_disp_mean,no_im] = image_eval(Folder,ext,
 %
 % NOTES
 % Please cite:
-% Landauer, A.K., Patel, M., Henann, D.L. et al. Exp Mech (2018).
+% Landauer, A.K., Patel, M., Henann, D.L. et al. Exp Mech (2018). 
 % https://doi.org/10.1007/s11340-018-0377-4
 
 % -------------------------------------------------------------------------
@@ -47,13 +47,17 @@ if l == 0
 else
     disp('Running image evaluation DIC')
     no_im = 0;
-
+    
     %read in the image sequence
     for ii = 1:l
         READ = imread(strcat(Folder,filesep,files(ii).name));
-        full_images(:,:,ii) = double(READ(:,:,1));
+        try 
+            full_images(:,:,ii) = double(rgb2gray(READ));
+        catch
+            full_images(:,:,ii) = double(READ);
+        end
     end
-
+    
     %find the bitdepth of the images
     S = whos('READ');
     if strcmp(S.class,'uint8')
@@ -64,7 +68,7 @@ else
         depth = max(full_images(:));
     end
     clear READ
-
+    
     %% Select eval region
     imagesc(full_images(:,:,1))
     title('Select noise evaluation ROI. Define two points: top left and bottom right')
@@ -73,46 +77,43 @@ else
     X = ceil(X);
     Y = ceil(Y);
     close
+    
 
-    try 
-      images = rgb2gray(full_images(Y(1):Y(2),X(1):X(2),:));
-    catch
-      images = full_images(Y(1):Y(2),X(1):X(2),:);
-    end
-
+    images = full_images(Y(1):Y(2),X(1):X(2),:);
+    
     %% Noise level
     %mean_image = mean(image,3);
     std_image = std(images,0,3);
-
+    
     noise_level = std(std_image(:));
-
+    
     noise_percent = noise_level/depth*100;
-
+    
     %% Spatial resolution
-
+    
     %set up parameters
     image_pair = cell(1,2);
     image_pair{1} = images(:,:,1);
     image_pair{2} = images(:,:,2);
-
+    
     u0 = cell(1,2);
     u0{1} = 0;
     u0{2} = 0;
 
-
+    
     %Do itereative DIC between the identical images
     [u,~,~,~] = IDIC(image_pair,sSize0,sSizeMin,u0);
-
+    
     %Compute spatial resolutions
     spatial_res(1) = nanstd(u{1}(:));
     spatial_res(2) = nanstd(u{2}(:));
     spatial_res(3) = nanstd(u{3}(:));
-
+    
     z = 1.96;
     %Compute the confidence interval on the displacements
     CI_disp(:,:,1) = u{3} - z*spatial_res(3)/sqrt(2);
     CI_disp_mean(1) = nanmean(nanmean(CI_disp(:,:,1)));
     CI_disp(:,:,2) = u{3} + z*spatial_res(3)/sqrt(2);
     CI_disp_mean(2) = nanmean(nanmean(CI_disp(:,:,2)));
-
+    
 end
